9bfdac099a7351e1c865e6b611e20fa42062898e
sana-ajani <sajani6@gatech.edu>
2020-09-08 11:12:35 -0400

    add assignment 3 fib.py

---
+++ 03-fibonacci/fib.py
@@ -0,0 +1,18 @@
+#recursive approach
+numTerms = int(input("How many terms of Fibonacci sequence to print? "))
+
+# main method
+def fibonacci(n):
+    if n < 1:
+        return n
+    else:
+        return(fibonacci(n-1) + fibonacci(n-2))
+
+
+# check if the number of terms is valid
+if numTerms <= 0:
+    print("Please enter a positive integer")
+else:
+    print("Fibonacci sequence:")
+    for i in range(numTerms):
+        print(fibonacci(i))
